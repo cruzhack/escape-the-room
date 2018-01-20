f704d87c3045fa92129782f64913cb920612432a
Sammy Franklin <sam@eduroam-169-233-199-30.ucsc.edu>
2018-01-20 03:47:24 -0800

    first commit; created basic staging, object, and movement mechanisms

---
+++ InteractiveObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour {
+
+	public string[] names;
+
+	public Transform placementPot;
+
+	public InteractiveObject[] childObjects {
+		get; set;
+	}
+
+	void Start(){
+
+	}
+
+	void Update(){
+		
+	}
+
+}
